a15da7ca24e2f65c15ea4bc68df3e6907fa79910
blipin <bartoszlipinski.szyb@gmail.com>
2024-01-15 17:33:41 +0100

    Fix starts and ends of lines in regex

--- linter.py
+++ linter.py
@@ -5,7 +5,7 @@ def verify_line(line: str) -> bool:
     if line.isspace():
         return True
 
-    return re.match(r"^[^\S\n]*(\S{1,} *(([+-|&^]{1}) *\S{1,} *)?-> *[^\s\,]{1,} *(, *\S{1,})? *(if(true|false|0|-|overflow|!0|!-|!overflow) {1,}go {1,}\S{1,})?)|(\/{2}.*$)|(^io {1,}\S* {0,}= {0,}\d*$)|(^\S* {0,}= {0,}\d*$)|(\S*:)|(go *\S{1,})$", line.strip(" \t")) is not None
+    return re.match(r"^([^\S\n]*(\S{1,} *(([+-|&^]{1}) *\S{1,} *)?-> *[^\s\,]{1,} *(, *\S{1,})? *(if(true|false|0|-|overflow|!0|!-|!overflow) {1,}go {1,}\S{1,})?)|(\/{2}.*)|(io {1,}\S* {0,}= {0,}\d*$)|(^\S* {0,}= {0,}\d{1,})|(\S*:)|(go *\S{1,}))$", line.strip(" \t")) is not None
 
 def lint_file(path):
     errors = []
